dcf787e7d549dd08fe420922df987de49739b610
Sergey <Sergey@localhost.localdomain>
2022-11-10 21:52:29 +0300

    Add index to Owner.name field

--- property/models.py
+++ property/models.py
@@ -85,7 +85,7 @@ class Claim(models.Model):
 
 
 class Owner(models.Model):
-    name = models.CharField('ФИО владельца', max_length=200)
+    name = models.CharField('ФИО владельца', max_length=200, db_index=True)
     phonenumber = models.CharField('Номер владельца', max_length=20)
     normilized_phonenumber = PhoneNumberField(
         'Нормализованный номер владельца',
