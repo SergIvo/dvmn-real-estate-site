0dbe6144ebe13f2aa6c374a091fc630a3c638a8e
Sergey <Sergey@localhost.localdomain>
2022-11-14 23:19:17 +0300

    Remove no longer existing field owner_pure_phone

--- property/admin.py
+++ property/admin.py
@@ -11,7 +11,7 @@ class OwnersInline(admin.TabularInline):
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('owner', 'town', 'address')
     readonly_fields = ('created_at',)
-    list_display = ('address', 'price', 'owner_pure_phone', 'new_building', 'construction_year', 'town')
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ('liked_by',)
